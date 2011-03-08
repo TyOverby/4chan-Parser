60c6b752b0a6dce33830ae9b4c0b4ed003393828
seth1010 <ty@pre-alpha.com>
2011-03-08 13:17:57 -0800

    made base files

---
+++ src/com/prealpha/EntryPoint.java
@@ -0,0 +1,15 @@
+package com.prealpha;
+
+public class EntryPoint
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/prealpha/ThreadParser.java
@@ -0,0 +1,6 @@
+package com.prealpha;
+
+public class ThreadParser
+{
+
+}
